ba1a737277af5aa4aefdd459081debeb48c6e9db
jstefanowski <jstefanowski@gmail.com>
2010-11-12 22:44:12 +0100

    Added intepreter of Ruby code

---
+++ onlinelab/service/engine/ruby/interpreter.rb
@@ -0,0 +1,46 @@
+require "/home/Kuba/femhub-online-lab/onlinelab/service/engine/ruby/outputtrap.rb"
+
+
+class RubyInterpreter
+
+    def initialize(locals={}, debug = false)
+        @debug = debug
+        @trap = OutputTrap.new
+        @index = 0
+    end
+
+    def complete(source)
+
+    end
+
+    def evaluate(source)
+        begin
+            traceback = false
+            @trap.set
+
+            start = Time.now.usec
+
+        begin
+            eval(source)
+        rescue Exception => ex
+            traceback = ex.message
+        end
+
+        stop  = Time.now.usec
+
+        @index += 1
+
+        return { "source" => source,
+            "index" => @index,
+            "time" => stop - start,
+            "out" => @trap.out,
+            "err" => @trap.err,
+            "plots" => [],
+            "traceback" => traceback,
+            "interrupted" => 0
+            }
+        ensure
+            @trap.reset
+        end
+    end
+end
