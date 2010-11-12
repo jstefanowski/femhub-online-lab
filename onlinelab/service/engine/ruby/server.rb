f569e9e6f5e5a1e91411fc2188244f2bd9f8d765
jstefanowski <jstefanowski@gmail.com>
2010-11-12 22:46:07 +0100

    Added classes responsible for handling xmlrpc

---
+++ onlinelab/service/engine/ruby/server.rb
@@ -0,0 +1,39 @@
+require 'webrick'
+require 'xmlrpc/server.rb'
+
+require "/home/Kuba/femhub-online-lab/onlinelab/service/engine/ruby/interpreter.rb"
+
+
+class RubyXMLRPCMethods
+    def initialize(interpreter)
+        @interpreter = interpreter
+    end
+  
+    def complete(source)
+        return @interpreter.complete(source)
+    end
+  
+    def evaluate(source)
+        return @interpreter.evaluate(source)
+    end
+end
+
+
+class RubyXMLRPCServer
+    @server
+    def initialize(port, interpreter)
+        methods = RubyXMLRPCMethods.new(RubyInterpreter.new)
+        servlet = XMLRPC::WEBrickServlet.new
+        servlet.add_handler("complete") { |source| methods.complete(source) }
+        servlet.add_handler("evaluate") { |source| methods.evaluate(source) }
+
+        @server=WEBrick::HTTPServer.new(:Port => port, :Logger =>
+        WEBrick::Log.new(nil) )
+        @server.mount("/", servlet)
+    end
+  
+    def serve_forever
+        $stdout.flush
+        @server.start
+    end
+end
