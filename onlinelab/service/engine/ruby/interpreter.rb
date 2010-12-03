7595811a8818ddf8694e90fc4fd11dc13edf004c
jstefanowski <jstefanowski@gmail.com>
2010-12-03 20:11:26 +0100

    Added getting object info in both short and full versions

--- onlinelab/service/engine/ruby/interpreter.rb
+++ onlinelab/service/engine/ruby/interpreter.rb
@@ -2,6 +2,7 @@ require 'rubygems'
 gem 'rdoc'
 require 'rdoc/ri/driver'
 
+require File.dirname(__FILE__) + "/inspector.rb"
 require File.dirname(__FILE__) + "/outputtrap.rb"
 
 
@@ -75,7 +76,11 @@ class RubyInterpreter
         begin
             last_logical = eval(source, @binding, @file_name)
         rescue Exception => ex
-            traceback = ex.message
+            if is_inspect(source)
+                return inspect(source)
+            else
+                traceback = ex.message
+            end
         end
 
         stop  = Time.now.usec
@@ -97,5 +102,64 @@ class RubyInterpreter
         ensure
             @trap.reset
         end
+    end
+
+    def is_inspect(source)
+        return source[0,1] == "?"
+    end
+
+    def inspect(source)
+        if source[0,2] == "??"
+            text = source[2..-1]
+            more = true
+        else
+            text = source[1..-1]
+            more = false
+        end
+
+        toInspect = text.split(".", -1)
+        
+	    begin
+            if toInspect.length > 1
+                front = toInspect[0]
+                back = toInspect[1..-1]
+
+                back = "." + back.join(".")
+            
+                if is_class(front)
+                    klass = front
+                else
+                    klass = eval(front+".class.to_s", @binding, @file_name)
+                end
+            else
+                if is_class(text)
+                    klass = text
+                else
+                    klass = eval(text+".class.to_s", @binding, @file_name)
+                end
+
+                back = ""
+            end
+
+            @index += 1
+            ins = Inspector.new
+            info = ins.get_pretty(klass.to_s + back, more)
+        rescue Exception => ex
+            info = "<no docstring>"
+        end
+
+        return {
+            'source' => source,
+            'text' => text,
+            'info' => info,
+            'more' => false,
+            'index' => @index,
+            'interrupted' => false
+            }
+    end
+
+    def is_class(text)
+        classList = eval("Class.constants", @binding, @file_name)
+        classList.include?(text)
     end
 end
